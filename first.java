f1b396f08f9c44ce48e29709f36998b284182683
PriyankaAgwan30 <148351905+PriyankaAgwan30@users.noreply.github.com>
2024-12-30 13:07:28 +0530

    Update first.java
    adding print statement

--- first.java
+++ first.java
@@ -1,5 +1,6 @@
 public class first {
 	public static void main(String[] args) {
 		System.out.println("Welcome to the CI/CD Pipeline Practical!");
+		System.out.println("Welcome to the CI/CD Pipeline Practical!");
 	}
 }
